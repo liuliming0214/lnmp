0256e2824656d82a321a90ba13c393ea132c874a
fallBirds <dev@pescms.com>
2014-04-26 23:17:29 +0800

    添加LNMP控制脚本

---
+++ status.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+echo "请选择您的操作:"
+echo "1. 重启Nginx"
+echo "2. 重启Mysql"
+echo "3. 重启PHP-FPM"
+echo "4. 重启LNMP"
+echo "5. 启动LNMP"
+echo "6. 停止LNMP"
+read num
+
+case "$num" in
+[1] ) 
+echo '执行关闭Nginx进程...'
+sudo kill -9 `ps -aux | grep nginx| awk -F' ' '{print $2}'`
+echo '进行重启Nginx...'
+sudo /opt/nginx/nginx
+echo '已成功重启Nginx!'
+;;
+[2] ) 
+echo '执行Mysql重启命令...'
+sudo /etc/init.d/mysql restart
+echo '已成功重启Mysql!'
+;;
+[3] )
+echo '执行关闭php-fpm进程...'
+sudo kill -9 `ps -aux | grep php-fpm| awk -F' ' '{print $2}'`
+echo '进行重启php-fpm...'
+sudo /opt/php/sbin/php-fpm
+echo '已成功重启php-fpm!'
+;;
+[4] ) 
+#执行顺序说明：Nginx执行PHP依赖php-fpm，因此必须先对php-fpm进行重启!
+echo '执行关闭php-fpm进程...'
+sudo kill -9 `ps -aux | grep php-fpm| awk -F' ' '{print $2}'`
+echo '进行重启php-fpm...'
+sudo /opt/php/sbin/php-fpm
+echo '已成功重启php-fpm!'
+echo '执行关闭Nginx进程...'
+sudo kill -9 `ps -aux | grep nginx| awk -F' ' '{print $2}'`
+echo '进行重启Nginx...'
+sudo /opt/nginx/nginx
+echo '已成功重启Nginx!'
+echo '执行Mysql重启命令...'
+sudo /etc/init.d/mysql restart
+echo '已成功重启Mysql!'
+echo 'LNMP已经成功重启！'
+;;
+[5] ) 
+echo '启动LNMP中...'
+sudo /opt/php/sbin/php-fpm
+sudo /opt/nginx/nginx
+sudo /etc/init.d/mysql start
+echo 'LNMP已经成功启动！'
+;;
+[6] ) 
+echo '停止LNMP中...'
+sudo kill -9 `ps -aux | grep php-fpm| awk -F' ' '{print $2}'`
+sudo kill -9 `ps -aux | grep nginx| awk -F' ' '{print $2}'`
+sudo /etc/init.d/mysql stop
+echo 'LNMP已经停止！'
+;;
+*) echo "没有任何操作，脚本退出";;
+esac
