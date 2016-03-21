a620cd908fdb2275fe01fa500ca929396dbdfc0f
luoboss <dev@pescms.com>
2016-03-21 17:11:55 +0800

    补充mysql5.5的安装脚本

--- centos6-lnmp.sh
+++ centos6-lnmp.sh
@@ -39,85 +39,91 @@ php="7.0.4"
 esac
 
 
-	#yum -y update
-	#nginx的编译环境
-	yum -y install gcc automake libtool make gcc gcc-c++ wget
+#yum -y update
+#nginx的编译环境
+yum -y install gcc automake libtool make gcc gcc-c++ wget
 
-	#mysql的编译环境
-	yum -y install ncurses-devel bison
+#mysql的编译环境
+yum -y install ncurses-devel bison
 
-	#PHP的编译环境
-	yum -y install cmake libxml2-devel bzip2-devel curl-devel libjpeg-devel libpng-devel autoconf freetype freetype-devel
+#PHP的编译环境
+yum -y install cmake libxml2-devel bzip2-devel curl-devel libjpeg-devel libpng-devel autoconf freetype freetype-devel
 
-	#下载要安装的文件
-	download(){
-	    cd ~
-	    wget --no-check-certificate https://www.pescms.com/lnmp/$1
-	    #确保文件能够被正确下载
-	    if [ ! -f $1 ]
-	    then
-	      download $1
-	    fi
-	}
-    #-------------------------------下载必要的软件----------------------------
-	download "pcre-8.36.tar.gz"
-	download "zlib-1.2.8.tar.gz"
-	download "openssl-1.0.2d.tar.gz"
-	download "bzip2-1.0.5.tar.gz"
-	download "nginx-${nginx}.tar.gz"
-	download "mysql-${mysql}.tar.gz"
-	download "php-${php}.tar.gz"
-    #-------------------------------下载对应版本的nmp安装脚本----------------------------
-    download "nginx/nginx-${nginx}.sh"
-    download "mysql/mysql-${mysql}.sh"
-    download "php/php-${php}.sh"
-    
-	#-------------------------------编译系统必要的文件----------------------------
-	cd ~
-	tar -xzvf pcre-8.36.tar.gz
-	mv pcre-8.36 /opt/pcre
-	cd /opt/pcre
-	./configure
-	make
-	make install
+#下载要安装的文件
+download(){
+    cd ~
+    if [ ! -f $1 ]
+    then
+        wget --no-check-certificate https://www.pescms.com/lnmp/$2$1
+    fi
+    #确保文件能够被正确下载
+    if [ ! -f $1 ]
+    then
+      download $1
+    fi
+}
 
-	cd ~
-	tar -zxvf zlib-1.2.8.tar.gz
-	mv zlib-1.2.8 /opt/zlib
-	cd /opt/zlib
-	./configure
-	make
-	make install
+#-------------------------------下载必要的软件----------------------------
+download "pcre-8.36.tar.gz"
+download "zlib-1.2.8.tar.gz"
+download "openssl-1.0.2d.tar.gz"
+download "bzip2-1.0.5.tar.gz"
+download "nginx-${nginx}.tar.gz"
+download "mysql-${mysql}.tar.gz"
+download "php-${php}.tar.gz"
+#-------------------------------下载对应版本的nmp安装脚本----------------------------
+download "nginx-${nginx}.sh" "nginx/"
+download "mysql-${mysql}.sh" "mysql/"
+download "php-${php}.sh" "php/"
 
-	cd ~
-	tar -zxvf openssl-1.0.2d.tar.gz
-	mv openssl-1.0.2d /opt/openssl
-    ./config shared zlib
-    make && make install
-    mv /usr/bin/openssl /usr/bin/openssl.old
-    mv /usr/include/openssl /usr/include/openssl.old
-    ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
-    ln -s /usr/local/ssl/include/openssl /usr/include/openssl
-    echo "/usr/local/ssl/lib" >> /etc/ld.so.conf
-    ldconfig -v
+#-------------------------------编译系统必要的文件----------------------------
+cd ~
+tar -xzvf pcre-8.36.tar.gz
+mv pcre-8.36 /opt/pcre
+cd /opt/pcre
+./configure
+make
+make install
 
-	cd ~
-	tar -zxvf bzip2-1.0.5.tar.gz
-	mv bzip2-1.0.5 /opt/bzip2
-	cd /opt/bzip2
-	make
-	make install
+cd ~
+tar -zxvf zlib-1.2.8.tar.gz
+mv zlib-1.2.8 /opt/zlib
+cd /opt/zlib
+./configure
+make
+make install
 
-    #-------------------------------安装Nginx----------------------------
-    sh "nginx-${nginx}.sh"
-    #-------------------------------安装Mysql----------------------------
-    sh "mysql-${mysql}.sh"
-    #-------------------------------安装PHP----------------------------
-    sh "php-${php}.sh"
-    
-	#创建访问目录
-	mkdir /var/www
-	#-------------------------------下载管理脚本------------------------------
-	wget --no-check-certificate https://www.pescms.com/lnmp/status.sh
-    echo "Install Complete!"
+cd ~
+tar -zxvf openssl-1.0.2d.tar.gz
+mv openssl-1.0.2d /opt/openssl
+#cd /opt/openssl
+#./config shared zlib
+#make && make install
+#mv /usr/bin/openssl /usr/bin/openssl.old
+#mv /usr/include/openssl /usr/include/openssl.old
+#ln -s /usr/local/ssl/bin/openssl /usr/bin/openssl
+#ln -s /usr/local/ssl/include/openssl /usr/include/openssl
+#echo "/usr/local/ssl/lib" >> /etc/ld.so.conf
+#ldconfig -v
+
+cd ~
+tar -zxvf bzip2-1.0.5.tar.gz
+mv bzip2-1.0.5 /opt/bzip2
+cd /opt/bzip2
+make
+make install
+
+cd ~
+#-------------------------------安装Nginx----------------------------
+sh "nginx-${nginx}.sh"
+#-------------------------------安装Mysql----------------------------
+sh "mysql-${mysql}.sh"
+#-------------------------------安装PHP----------------------------
+sh "php-${php}.sh"
+
+#创建访问目录
+mkdir /var/www
+#-------------------------------下载管理脚本------------------------------
+wget --no-check-certificate https://www.pescms.com/lnmp/status.sh
+echo "Install Complete!"
 
--- mysql/mysql-5.5.47.sh
+++ mysql/mysql-5.5.47.sh
@@ -1,1 +1,46 @@
 #!/bin/bash
+	cd ~
+	groupadd mysql
+	useradd -g mysql mysql -s /usr/sbin/nologin
+
+	tar -xvzf mysql-5.5.47.tar.gz
+	cd mysql-5.5.47
+    
+    mkdir /opt/mysql/
+	mkdir /opt/mysql/data
+	mkdir /opt/mysql/log
+
+	cmake -DCMAKE_INSTALL_PREFIX=/opt/mysql -DMYSQL_UNIX_ADDR=/opt/mysql/mysql.sock -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DWITH_MYISAM_STORAGE_ENGINE=1 -DWITH_INNOBASE_STORAGE_ENGINE=1 -DWITH_READLINE=1 -DENABLED_LOCAL_INFILE=1 -DMYSQL_DATADIR=/opt/mysql/data -DMYSQL_TCP_PORT=3306
+	make
+	make install
+
+    
+	cd /opt/mysql
+	#赋予权限，避免执行时出错
+	chmod -R 777 /opt/mysql/scripts/mysql_install_db
+
+
+	/opt/mysql/scripts/mysql_install_db --basedir=/opt/mysql --datadir=/opt/mysql/data --user=mysql
+	cp -uf support-files/my-large.cnf  /etc/my.cnf
+
+	#将MYSQL的配置信息写入配置文件
+
+	#配置用户权限
+	sudo chown -R mysql:mysql /opt/mysql
+
+	cp /opt/mysql/support-files/mysql.server /etc/init.d/mysql
+
+	#赋予权限，避免执行出错
+	sudo chmod -R 777 /etc/init.d/mysql
+
+	#清空一次存在的系统变量
+	sed  -i 's/export PATH=.*$//g' /etc/profile
+	#将Mysql写入环境变量
+	echo "export PATH=/opt/mysql/bin:$PATH" >>/etc/profile
+
+	#重新加载环境变量
+	. /etc/profile
+
+	/etc/init.d/mysql start
+
+	mysqladmin -u root password "123456" #设置root密码
