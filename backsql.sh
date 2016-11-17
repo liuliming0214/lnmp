98503e76511a019b031891fb24c307285c5ae468
luoboss <dev@pescms.com>
2016-11-17 15:52:39 +0800

    修复备份写错后缀的问题

--- backsql.sh
+++ backsql.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 filename=`date +%y-%m-%d`
-/opt/mysql/bin/mysqldump -uroot -p123456 dbname>/home/backup/dbname-${filename}.sq
+/opt/mysql/bin/mysqldump -uroot -p123456 dbname>/home/backup/dbname-${filename}.sql
 find /home/backup -mtime +10 -name "*.sql" -exec rm -rf {} \;
